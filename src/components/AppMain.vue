6306fc8d1041757a42bf9aa57ad20bc480e621aa
Drew0004 <drdrewgraphic@gmail.com>
2024-01-18 17:00:13 +0100

    Add blue section objects data

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,6 +2,28 @@
 export default {
     data() {
         return {
+            blueItems : [
+                {
+                    image: '../../public/vue-dc-comics-1/img/buy-comics-digital-comics.png',
+                    caption: 'DIGITAL COMICS'
+                },
+                {
+                    image: '../../public/vue-dc-comics-1/img/buy-comics-merchandise.png',
+                    caption: 'DC MERCHANDISE'
+                },
+                {
+                    image: '../../public/vue-dc-comics-1/img/buy-comics-subscriptions.png',
+                    caption: 'SUBSCRIPTION'
+                },
+                {
+                    image: '../../public/vue-dc-comics-1/img/buy-comics-shop-locator.png',
+                    caption: 'COMIC SHOP LOCATOR'
+                },
+                {
+                    image: '../../public/vue-dc-comics-1/img/buy-dc-power-visa.svg',
+                    caption: 'DC POWER VISA'
+                },
+            ]
 
         };
     },
@@ -13,9 +35,19 @@ export default {
 
 <template>
     <main>
-        <div>
-            <h2>Content goes here </h2>
-        </div>
+        <section class="main-gray">
+            <div>
+                <h2>Content goes here </h2>
+            </div>
+        </section>
+
+        <section class="blue-main">
+            <div>
+                content
+            </div>
+        </section>
+
+
     </main>
 </template>
 
@@ -24,17 +56,31 @@ export default {
     @use "../assets/scss/partials/mixins.scss" as *;
     
     main{
-        background-color: $main_background_color;
-        padding: $spacing_50;
+        .main-gray{
+            background-color: $main_background_color;
+            padding: $spacing_50;
         
-        div{
-            @include container;
+            >div{
+                @include container;
 
-            h2{
-                font-size: $font_size_large;
-                font-weight: $font_bold;
-                color: $white_color;
+                h2{
+                    font-size: $font_size_large;
+                    font-weight: $font_bold;
+                    color: $white_color;
+                }
             }
         }
+
+        .blue-main{
+            background-color: $blue_brand;
+            padding: $spacing_50;
+            
+            >div{
+                @include container;
+            }
+        }
+        
+    
     }
+
 </style>
